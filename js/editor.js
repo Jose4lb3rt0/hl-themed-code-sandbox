73d960211c01b8ff5115ae06dd3cf91eecdb83f6
Jose4lb3rt0 <josealberto3200@gmail.com>
2025-10-17 19:06:57 -0500

    Ruta editor corregida

--- js/editor.js
+++ js/editor.js
@@ -120,7 +120,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     require.config({ paths: { 'vs': 'https://cdn.jsdelivr.net/npm/monaco-editor@0.53.0/min/vs' } })
     require(['vs/editor/editor.main'], function () {
-        fetch('../libs/steam-classic.json')
+        fetch('./libs/steam-classic.json')
             .then(res => res.json())
             .then(themeJson => {
                 const monacoTheme = vscodeThemeToMonaco(themeJson)
